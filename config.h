d02c869326229aa8ef67c8e26b27ad541d4c93d1
qasimwarraich <qasim.warraich@gmail.com>
2020-07-11 17:19:08 +0200

    Updated terminal dimensions

--- config.h
+++ config.h
@@ -149,8 +149,8 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+static unsigned int cols = 100;
+static unsigned int rows = 35;
 
 /*
  * Default colour and shape of the mouse cursor
