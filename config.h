fd041c44d900300b5b42ff6a79c1844ecf079938
qasimwarraich <qasim.warraich@gmail.com>
2020-07-16 23:59:44 +0200

    more font fixes libxft-bgra installed via AUR

--- config.h
+++ config.h
@@ -8,7 +8,7 @@
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 static char *font2[] = {
-    "JoyPixels:pixelsize=10:antialias=true:autohint=true",
+    "JoyPixels:pixelsize=10:antialias=true:autohint=true"
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
